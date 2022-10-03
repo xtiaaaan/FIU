5988cdbe4e2d50106d7bf9cfa50103d236c27de3
xtiaaaan <nico.angus3@gmail.com>
2022-10-03 12:01:25 -0400

    Created HW3 COP4226

--- COP 4226/COP4226_Assignment2_Minesweeper/COP4226_Assignment2_Minesweeper/Form3.cs
+++ COP 4226/COP4226_Assignment2_Minesweeper/COP4226_Assignment2_Minesweeper/Form3.cs
@@ -22,22 +22,6 @@ namespace COP4226_Assignment2_Minesweeper
 
         private void okay_Click(object sender, EventArgs e)
         {
-            int value;
-            if(!int.TryParse(rowBox.Text, out value) || !int.TryParse(columnBox.Text, out value) || !int.TryParse(minesBox.Text, out value))
-            {
-                MessageBox.Show("Please enter a valid integer", "Invalid Input", MessageBoxButtons.OK);
-            }
-            else if(int.Parse(rowBox.Text) <= 0 || int.Parse(columnBox.Text) <= 0 || int.Parse(minesBox.Text) <= 0)
-            {
-                MessageBox.Show("Please enter an integer greater than 0", "No zeroes and negative numbers", MessageBoxButtons.OK);
-            }
-            else
-            {
-                row = int.Parse(rowBox.Text);
-                col = int.Parse(columnBox.Text);
-                mines = int.Parse(minesBox.Text);
-                this.DialogResult = System.Windows.Forms.DialogResult.OK;
-            }
             this.Close();
         }
     }
---
+++ COP 4226/Class Activity VII/Class Activity VII/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿
+namespace Class_Activity_VII
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.increaseCountToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.decreaseCountToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.fromDate = new System.Windows.Forms.DateTimePicker();
+            this.toDate = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(10, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(564, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.increaseCountToolStripMenuItem,
+            this.decreaseCountToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // increaseCountToolStripMenuItem
+            // 
+            this.increaseCountToolStripMenuItem.Name = "increaseCountToolStripMenuItem";
+            this.increaseCountToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.increaseCountToolStripMenuItem.Text = "Increase Count";
+            this.increaseCountToolStripMenuItem.Click += new System.EventHandler(this.increaseCountToolStripMenuItem_Click);
+            // 
+            // decreaseCountToolStripMenuItem
+            // 
+            this.decreaseCountToolStripMenuItem.Name = "decreaseCountToolStripMenuItem";
+            this.decreaseCountToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+            this.decreaseCountToolStripMenuItem.Text = "Decrease Count";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.numericUpDown1.Location = new System.Drawing.Point(257, 157);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(53, 23);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // fromDate
+            // 
+            this.fromDate.Location = new System.Drawing.Point(30, 65);
+            this.fromDate.Margin = new System.Windows.Forms.Padding(20);
+            this.fromDate.Name = "fromDate";
+            this.fromDate.Size = new System.Drawing.Size(200, 23);
+            this.fromDate.TabIndex = 2;
+            this.fromDate.ValueChanged += new System.EventHandler(this.fromDate_ValueChanged);
+            // 
+            // toDate
+            // 
+            this.toDate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.toDate.Location = new System.Drawing.Point(354, 65);
+            this.toDate.Margin = new System.Windows.Forms.Padding(20);
+            this.toDate.Name = "toDate";
+            this.toDate.Size = new System.Drawing.Size(200, 23);
+            this.toDate.TabIndex = 3;
+            this.toDate.ValueChanged += new System.EventHandler(this.fromDate_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(106, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "From Date";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(413, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "To Date";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(264, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Count";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 236);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.toDate);
+            this.Controls.Add(this.fromDate);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(10, 0, 10, 10);
+            this.Text = "Day Counter";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem increaseCountToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem decreaseCountToolStripMenuItem;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.DateTimePicker fromDate;
+        private System.Windows.Forms.DateTimePicker toDate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
