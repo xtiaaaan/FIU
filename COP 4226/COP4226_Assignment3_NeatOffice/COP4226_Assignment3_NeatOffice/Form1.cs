d935bd045d9aa6552dc2c38620a81ec18ddc4382
xtiaaaan <nico.angus3@gmail.com>
2022-10-07 02:33:41 -0400

    Restarted HW3 COP4226

---
+++ COP 4226/COP4226_Assignment3_NeatOffice/COP4226_Assignment3_NeatOffice/Form1.cs
@@ -0,0 +1,10 @@
+namespace COP4226_Assignment3_NeatOffice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
